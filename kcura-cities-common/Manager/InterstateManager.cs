1da1ae15706664076e56d8542a57e0967cadb624
Fred Merlo <fredmerlo@gmail.com>
2015-08-18 16:08:55 -0600

    added interstate manager and initial test

---
+++ kcura-cities-common/Manager/InterstateManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using kcura_cities_common.Models;
+
+namespace kcura_cities_common.Manager
+{
+    public class InterstateManager
+    {
+        public List<Interstate> Interstates { get; set; }
+    }
+}
---
+++ kcura-cities-tests/InterstateManagerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using kcura_cities_common.Manager;
+using kcura_cities_common.Models;
+using Xunit;
+
+namespace kcura_cities_tests
+{
+    public class InterstateManagerFixture
+    {
+        public List<Interstate> InterstateGroupA { get; set; }
+
+        public InterstateManagerFixture()
+        {
+            InterstateManager = new InterstateManager{Interstates = new List<Interstate>()};
+        }
+
+        public InterstateManager InterstateManager { get; set; }
+    }
+
+    public class InterstateManagerTests : IClassFixture<InterstateManagerFixture>
+    {
+        private InterstateManagerFixture fixture;
+
+        public InterstateManagerTests(InterstateManagerFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+        [Fact]
+        public void InterstateManagerContainsListOfInterstate()
+        {
+            Assert.IsType<List<Interstate>>(fixture.InterstateManager.Interstates);
+        }
+    }
+}
