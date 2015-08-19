94fbca6ae6bdbb21cf691853de682e34c21e5bef
Fred Merlo <fredmerlo@gmail.com>
2015-08-18 18:05:00 -0600

    Added distance manager and initial tests

---
+++ kcura-cities-common/Manager/DistanceManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using kcura_cities_common.Models;
+
+namespace kcura_cities_common.Manager
+{
+    public class DistanceManager
+    {
+        public Dictionary<string, City> Cities { get; set; }
+        public Dictionary<string, Interstate> Interstates { get; set; }
+    }
+}
---
+++ kcura-cities-tests/DistanceManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using kcura_cities_common.Manager;
+using kcura_cities_common.Models;
+using Xunit;
+
+namespace kcura_cities_tests
+{
+    public class DistanceManagerTests
+    {
+        [Fact]
+        public void DistanceManagerContainsDictionaryOfCity()
+        {
+            var distanceManager = new DistanceManager{Cities = new Dictionary<string, City>()};
+
+            Assert.IsType<Dictionary<string, City>>(distanceManager.Cities);
+        }
+
+        [Fact]
+        public void DistanceManagerContainsDictionaryOfInterstate()
+        {
+            var distanceManager = new DistanceManager {Interstates = new Dictionary<string, Interstate>()};
+
+            Assert.IsType<Dictionary<string, Interstate>>(distanceManager.Interstates);
+        }
+
+
+    }
+}
