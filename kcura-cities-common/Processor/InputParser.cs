72a0ce7a628b61ec08de8268f8d68e148f001c1d
Fred Merlo <fredmerlo@gmail.com>
2015-08-20 13:11:51 -0600

    Initial input parser and tests

---
+++ kcura-cities-common/Processor/InputParser.cs
@@ -0,0 +1,16 @@
+﻿using kcura_cities_common.Models;
+
+namespace kcura_cities_common.Processor
+{
+    public class InputParser
+    {
+        public City ParseItemForCity(string item)
+        {
+            var parts = item.Split('|');
+            var population = 0;
+            int.TryParse(parts[0], out population);
+
+            return new City {Name = parts[1], State = parts[2], Population = population};
+        }
+    }
+}
---
+++ kcura-cities-tests/InputParserTests.cs
@@ -0,0 +1,23 @@
+﻿using kcura_cities_common.Models;
+using kcura_cities_common.Processor;
+using Xunit;
+
+namespace kcura_cities_tests
+{
+    public class InputParserTests
+    {
+        [Fact]
+        public void InputParserCreatesCityFromPipeContent()
+        {
+            var content = "6|Oklahoma City|Oklahoma|I-35;I-44;I-40";
+            var parser = new InputParser();
+            var expected = new City { Name = "Oklahoma City", State = "Oklahoma", Population = 6 };
+
+            var actual = parser.ParseItemForCity(content);
+
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.State, actual.State);
+            Assert.Equal(expected.Population, actual.Population);
+        }
+    }
+}
